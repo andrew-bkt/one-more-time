2d09eb3805aa4b3e576786dc7762d9b6b5f01fb6
andrew-bkt <andrew@brooklyn-techology.com>
2024-07-29 02:49:14 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import './globals.css';
