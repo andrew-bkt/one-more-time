e9f71ea66c7c40c81bcae36dc84b54b864cf8a13
andrew-bkt <andrew@brooklyn-techology.com>
2024-07-29 02:43:15 -0400

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Layout from './layout';
 import { NavHero } from '../components/nav-hero';
+import { ProductFeature } from '../components/product-feature';
+import { HomePoints } from '../components/home-points';
+import { HomeFooter } from '../components/home-footer';
 
 export default function Page() {
   return (
     <Layout>
       <NavHero />
+      <HomePoints />
+      <ProductFeature />
+      <HomeFooter />
     </Layout>
   );
 }
